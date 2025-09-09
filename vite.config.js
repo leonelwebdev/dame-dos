7ad9c5033e3a0e82fa0e3c9f0088f7a8acaa8153
leonelwebdev <leonelrodrigguez@gmail.com>
2025-09-09 20:55:20 -0300

    dejo instalado tailwind, config dea aliases, y preparo vite config para pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Stacking App",
+        short_name: "Stacking",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0d6efd",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@data': path.resolve(__dirname, 'src/data'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@sections': path.resolve(__dirname, 'src/sections'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    }
+  },
+  envDir: path.resolve(__dirname, "../"),
+  build: {
+    outDir: 'dist',
+  }
 })
